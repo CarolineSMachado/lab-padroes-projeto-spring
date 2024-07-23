4637e06f8fd81bd809ebc459de72a67098d467e2
CarolineSMachado <caroline.machadobz@gmail.com>
2024-07-22 22:46:05 -0300

    Projetos Spring Java, atividade DIO

--- demo/src/main/java/dio/projeto/demo/DemoApplication.java
+++ demo/src/main/java/dio/projeto/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package dio.projeto.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 public class DemoApplication {
--- demo/src/main/java/service/ClienteService.java
+++ demo/src/main/java/service/ClienteService.java
@@ -1,5 +1,17 @@
 package service;
 
-public class ClienteService {
+import model.Cliente;
+
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
 
 }
